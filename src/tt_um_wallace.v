4fced121d547175d5dec919d24e71a97f3801aa6
Ajimenezph <69359121+Ajimenezph@users.noreply.github.com>
2024-06-29 12:21:04 -0600

    Add files via upload

--- src/tt_um_wallace.v
+++ src/tt_um_wallace.v
@@ -54,7 +54,7 @@ assign Data_out_Sum = ha2_sum;
 //The carry's from both the half adders are OR'ed to get the final carry./
 assign Data_out_Carry = ha1_carry | ha2_carry;
 endmodule
-module tt_um_wallace(A,B,prod);
+module wallace(A,B,prod);
 //inputs and outputs
 input [3:0] A,B;
 output [7:0] prod;
